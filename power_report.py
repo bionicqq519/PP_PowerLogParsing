231ab0054966bd03042bcc7006d08961721d3dea
NickTJLee <NickTJLee@altek.com.tw>
2022-02-07 21:46:00 +0800

    python flask upload and download file and csv parsing first commit.

---
+++ power_report.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 30 15:37:27 2022
+
+@author: Nick
+"""
+import os
+from flask import Flask, render_template, request, send_from_directory
+from werkzeug.utils import secure_filename
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Flask on port 8888."
+
+@app.route("/power")
+def power():
+    return render_template("./power_upload.html")
+
+def parseCSV(file):
+    df = pd.read_csv(file)
+    msg = '<table>'
+    msg += '<tr>'
+    msg += '    <th>Power-IA Core Power(Watts) mean.</th>'
+    msg += '    <th>Power-Integrated Graphics Power(Watts) mean.</th>'
+    msg += '<tr>'
+    msg += '<tr>'
+    msg += ('    <td>'+ str(df['Power-IA Core Power(Watts)'].mean()) +'</td>')
+    msg += ('    <td>'+ str(df['Power-Integrated Graphics Power(Watts)'].mean()) +'</td>')
+    msg += '<tr>'
+    msg += '</table>'
+    
+    # print(msg)
+    
+    return msg
+
+@app.route("/upload_show", methods=['GET', 'POST'])
+def upload_show():
+    msg = "file uploaded failed"
+    
+    if request.method == 'POST':
+        file = request.files['file']
+        # read by pandas
+        mean_msg = parseCSV(file)
+        
+        filename = secure_filename(file.filename)
+        if filename == '':
+            return "file is empty"
+        filepath = os.path.join('./upload', filename)
+        #file.save(filepath)
+        msg = "file uploaded successfully. path = " + filepath + "<br>"
+        msg += mean_msg
+        #msg += ('<a href="/download/' + filename + '">Download file</a><br>')
+    else:
+        msg = "file uploaded failed"
+        
+    return msg
+
+@app.route("/download2/<path:filename>")
+def downloader(filename):
+	dirpath = os.path.join(app.root_path, 'upload')
+	return send_from_directory(dirpath, filename, as_attachment=True)
+
+@app.route('/download/<path:filename>')
+def download_file(filename):
+    file_path =  os.path.join(app.root_path, 'upload')
+    file_path = os.path.join(file_path, filename)
+    file_handle = open(file_path, 'r')
+    
+    print(filename)
+
+    # This *replaces* the `remove_file` + @after_this_request code above
+    def stream_and_remove_file():
+        yield from file_handle
+        file_handle.close()
+        os.remove(file_path)
+
+    return app.response_class(
+        stream_and_remove_file(),
+        headers={'Content-Disposition': 'attachment', 'filename': filename}
+    )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8888)
